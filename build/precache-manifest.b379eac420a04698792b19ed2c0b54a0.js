e639cc09d74b30b74b87fde6e9fd60b5cc97b427
Damian Zapalski <damyanz@MacBook-Pro.local>
2020-09-16 20:35:31 +0200

    build app

---
+++ build/precache-manifest.b379eac420a04698792b19ed2c0b54a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa51064ce45dfeba16187d59b5afdeac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ca27f1a7b73219d367",
+    "url": "/static/css/main.9261f841.chunk.css"
+  },
+  {
+    "revision": "6d46b4bba7dcb269533e",
+    "url": "/static/js/2.1baf4319.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1baf4319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ca27f1a7b73219d367",
+    "url": "/static/js/main.405b90df.chunk.js"
+  },
+  {
+    "revision": "2c5e2cebed0ab231717d",
+    "url": "/static/js/runtime-main.6e49ef98.js"
+  }
+]);
